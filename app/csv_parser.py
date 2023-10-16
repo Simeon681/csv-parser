d74a45106c8e6492adbb3af892636f0fb9ec9b2b
Simeon Georgiev Angelov <107799217+Simeon681@users.noreply.github.com>
2023-10-16 18:54:43 +0300

    mock done

--- app/csv_parser.py
+++ app/csv_parser.py
@@ -1,17 +1,17 @@
 import csv
 import statistics
 
-class CSVParser:
-    def __init__(self, file_path):
-        self.data = self.parse_csv(file_path)
+def parse_csv(file_path):
+    data = []
+    with open(file_path, 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            data.append(row)
+    return data
 
-    def parse_csv(self, file_path):
-        data = []
-        with open(file_path, 'r') as csvfile:
-            csv_reader = csv.DictReader(csvfile)
-            for row in csv_reader:
-                data.append(row)
-        return data
+class CSVParser:
+    def __init__(self, data):
+        self.data = data
 
     def count_rows(self):
         return len(self.data)
@@ -27,20 +27,40 @@ class CSVParser:
 
     def find_min(self, column_name):
         values = [float(row[column_name]) for row in self.data if row[column_name].strip()]
-        return min(values)
+        minimum = min(values)
+        
+        assert minimum != None, "Minimum should not be None"
+
+        return minimum
 
     def find_max(self, column_name):
         values = [float(row[column_name]) for row in self.data if row[column_name].strip()]
-        return max(values)
+        maximum = max(values)
+
+        assert maximum != None, "Maximum should not be None"
+
+        return maximum
 
     def find_avg(self, column_name):
         values = [float(row[column_name]) for row in self.data if row[column_name].strip()]
-        return statistics.mean(values)
+        avg = statistics.mean(values)
+
+        assert avg != None, "Average should not be None"
+
+        return avg
 
     def find_shortest_string(self, column_name):
         values = [row[column_name] for row in self.data if row[column_name].strip()]
-        return min(values, key=len)
+        shortest_string = min(values, key=len)
+
+        assert shortest_string != None, "Shortest string should not be None"
+
+        return shortest_string
 
     def find_longest_string(self, column_name):
         values = [row[column_name] for row in self.data if row[column_name].strip()]
-        return max(values, key=len)
+        longest_strng = max(values, key=len)
+
+        assert longest_strng != None, "Longest string should not be None"
+
+        return longest_strng
