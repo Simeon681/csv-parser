f3b8942597c9382cf7f879c2fd93a242136ba780
Simeon Georgiev Angelov <107799217+Simeon681@users.noreply.github.com>
2023-11-13 17:40:45 +0200

    benchmark

--- tests/test_csv_parser.py
+++ tests/test_csv_parser.py
@@ -2,8 +2,7 @@ import pytest
 import csv
 from statistics import StatisticsError
 from app.csv_parser import CSVParser, parse_csv
-
-#FILE_PATH = "./data/example.csv"
+# FILE_PATH = "./data/example.csv"
 
 CSV_DATA = """Name,City,Age
 Simeon,Sofia,25
@@ -12,16 +11,17 @@ Bobi,Veliko Tarnovo,39
 """
 
 INVALID_AGE = [
-    {'Name': 'Simeon', 'City': 'Sofia', 'Age': ''},
-    {'Name': 'Viktor', 'City': 'Plovdiv', 'Age': ''},
-    {'Name': 'Bobi', 'City': 'Veliko Tarnovo', 'Age': ''}
+    {"Name": "Simeon", "City": "Sofia", "Age": ""},
+    {"Name": "Viktor", "City": "Plovdiv", "Age": ""},
+    {"Name": "Bobi", "City": "Veliko Tarnovo", "Age": ""},
 ]
 
-INVALID_STRINGS = [    
-    {'Name': '', 'City': '', 'Age': '25'},
-    {'Name': '', 'City': '', 'Age': '35'},
-    {'Name': '', 'City': '', 'Age': '39'}
+INVALID_STRINGS = [
+    {"Name": "", "City": "", "Age": "25"},
+    {"Name": "", "City": "", "Age": "35"},
+    {"Name": "", "City": "", "Age": "39"},
 ]
+
 
 @pytest.fixture
 def mock_open(mocker):
@@ -29,64 +29,74 @@ def mock_open(mocker):
     mocker.side_effect = csv.Error("File is not a valid CSV file")
     mocker.patch("builtins.open", mocker_open_csv)
 
+
 def test_parse_csv_with_fixture(mocker, mock_open):
-    data = parse_csv('file_path.csv')
+    data = parse_csv("file_path.csv")
     expected_data = [
-        {'Name': 'Simeon', 'City': 'Sofia', 'Age': '25'},
-        {'Name': 'Viktor', 'City': 'Plovdiv', 'Age': '35'},
-        {'Name': 'Bobi', 'City': 'Veliko Tarnovo', 'Age': '39'}
+        {"Name": "Simeon", "City": "Sofia", "Age": "25"},
+        {"Name": "Viktor", "City": "Plovdiv", "Age": "35"},
+        {"Name": "Bobi", "City": "Veliko Tarnovo", "Age": "39"},
     ]
 
     assert data == expected_data
 
+
 @pytest.fixture
 def parser(mocker, mock_open):
-    return CSVParser(parse_csv('file_path.csv'))
+    return CSVParser(parse_csv("file_path.csv"))
+
 
 def test_parse_csv_with_invalid_file():
     with pytest.raises(ValueError) as e:
-        parse_csv('file_path.txt')
+        parse_csv("file_path.txt")
 
     assert e.type == ValueError
+
 
 def test_parse_csv_with_empty_file():
     with pytest.raises(ValueError) as e:
-        parse_csv('')
+        parse_csv("")
 
     assert e.type == ValueError
+
 
 def test_count_rows(parser):
     assert parser.count_rows() == 3
 
+
 def test_count_rows_throws_exception(parser):
     parser.data = [
-        {'Name': 'Simeon', 'City': 'Sofia', 'Age': '25'},
-        {'Name': 'Viktor', 'City': 'Plovdiv', 'Age': '35'}
+        {"Name": "Simeon", "City": "Sofia", "Age": "25"},
+        {"Name": "Viktor", "City": "Plovdiv", "Age": "35"},
     ]
     with pytest.raises(AssertionError) as e:
         parser.count_rows()
 
     assert e.type == AssertionError
 
+
 def test_count_columns(parser):
     assert parser.count_columns() == 3
 
+
 def test_count_columns_throws_exception(parser):
     parser.data = [
-        {'Name': 'Simeon', 'Age': '25'},
-        {'Name': 'Viktor', 'Age': '35'},
-        {'Name': 'Bobi', 'Age': '39'}
+        {"Name": "Simeon", "Age": "25"},
+        {"Name": "Viktor", "Age": "35"},
+        {"Name": "Bobi", "Age": "39"},
     ]
     with pytest.raises(AssertionError) as e:
         parser.count_columns()
 
     assert e.type == AssertionError
 
+
 def test_column_exists(parser):
-    assert parser.column_exists("Name") == True
-    assert parser.column_exists("Age") == True
-    assert parser.column_exists("City") == True
-    assert parser.column_exists("Country") == False
+    assert parser.column_exists("Name") is True
+    assert parser.column_exists("Age") is True
+    assert parser.column_exists("City") is True
+    assert parser.column_exists("Country") is False
+
 
 def test_column_exists_with_empty_column_name(parser):
     with pytest.raises(ValueError) as e:
@@ -94,8 +104,11 @@ def test_column_exists_with_empty_column_name(parser):
 
     assert e.type == ValueError
 
-def test_column_is_numeric(parser):
-    assert parser.column_is_numeric("Age") == True
+
+def test_column_is_numeric(parser, benchmark):
+    result = benchmark(parser.column_is_numeric, "Age")
+    assert result is True
+
 
 def test_column_is_numeric_with_empty_column_name(parser):
     with pytest.raises(ValueError) as e:
@@ -103,14 +116,18 @@ def test_column_is_numeric_with_empty_column_name(parser):
 
     assert e.type == ValueError
 
-def test_sum_column(parser):
-    assert parser.sum_column("Age") == 99.0
+
+def test_sum_column(parser, benchmark):
+    result = benchmark(parser.sum_column, "Age")
+    assert result == 99.0
+
 
 def test_sum_column_with_empty_column_name(parser):
     with pytest.raises(ValueError) as e:
         parser.sum_column("")
 
     assert e.type == ValueError
+
 
 def test_sum_column_throws_exception(parser):
     with pytest.raises(ValueError) as e:
@@ -119,14 +136,19 @@ def test_sum_column_throws_exception(parser):
 
     assert e.type == ValueError
 
-def test_find_min(parser):
-    assert parser.find_min("Age") == 25.0
+
+def test_find_min(parser, benchmark):
+    result = benchmark(parser.find_min, "Age")
+    assert result == 25.0
+
 
 def test_find_max(parser):
     assert parser.find_max("Age") == 39.0
 
+
 def test_find_avg(parser):
     assert parser.find_avg("Age") == 33.0
+
 
 def test_find_min_throws_exception(parser):
     with pytest.raises(ValueError) as e:
@@ -134,11 +156,13 @@ def test_find_min_throws_exception(parser):
 
     assert e.type == ValueError
 
+
 def test_find_max_throws_exception(parser):
     with pytest.raises(ValueError) as e:
         parser.find_max("City")
 
     assert e.type == ValueError
+
 
 def test_find_avg_throws_exception(parser):
     with pytest.raises(ValueError) as e:
@@ -154,12 +178,14 @@ def test_find_min_with_empty_values(parser):
 
     assert e.type == ValueError
 
+
 def test_find_max_with_empty_values(parser):
     parser.data = INVALID_AGE
     with pytest.raises(ValueError) as e:
         parser.find_max("Age")
 
     assert e.type == ValueError
+
 
 def test_find_avg_with_empty_values(parser):
     parser.data = INVALID_AGE
@@ -168,11 +194,14 @@ def test_find_avg_with_empty_values(parser):
 
     assert e.type == StatisticsError
 
+
 def test_find_shortest_string(parser):
     assert parser.find_shortest_string("Name") == "Bobi"
 
+
 def test_find_longest_string(parser):
     assert parser.find_longest_string("City") == "Veliko Tarnovo"
+
 
 def test_find_shortest_string_with_empty_values(parser):
     parser.data = INVALID_STRINGS
@@ -180,6 +209,7 @@ def test_find_shortest_string_with_empty_values(parser):
         parser.find_shortest_string("Name")
 
     assert e.type == ValueError
+
 
 def test_find_longest_string_with_empty_values(parser):
     parser.data = INVALID_STRINGS
