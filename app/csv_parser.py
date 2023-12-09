428592e000329bf2098f047dfa8a41324214e601
Simeon Georgiev Angelov <107799217+Simeon681@users.noreply.github.com>
2023-12-09 22:42:41 +0200

    Final

--- app/csv_parser.py
+++ app/csv_parser.py
@@ -1,46 +1,116 @@
 import csv
 import statistics
+from cProfile import Profile
+from pstats import SortKey, Stats
+
+def parse_csv(file_path):
+    data = []
+    if not file_path.endswith('.csv'):
+        raise ValueError("File should be .csv")
+    elif not file_path:
+        raise ValueError("File path should not be empty")  
+    with open(file_path, 'r') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            data.append(row)
+    return data
 
 class CSVParser:
-    def __init__(self, file_path):
-        self.data = self.parse_csv(file_path)
+    def __init__(self, data):
+        self.data = data
 
-    def parse_csv(self, file_path):
-        data = []
-        with open(file_path, 'r') as csvfile:
-            csv_reader = csv.DictReader(csvfile)
-            for row in csv_reader:
-                data.append(row)
-        return data
 
     def count_rows(self):
-        return len(self.data)
+        rows_len = len(self.data)
+
+        assert rows_len == 3, "Rows length should not be greater or less than 3"
+        assert rows_len != None, "Rows length should not be None"
+
+        return rows_len
+    
+
+    def count_columns(self):
+        columns_len = len(self.data[0].keys())
+
+        assert columns_len == 3, "Columns length should not be greater or less than 3"
+        assert columns_len != None, "Columns length should not be None"
+
+        return columns_len
+    
+
+    def column_exists(self, column_name):
+        if not column_name:
+            raise ValueError("Column name should not be empty")
+        for row in self.data:
+            if column_name in row:
+                return True
+        return False
+    
+
+    def column_is_numeric(self, column_name):
+        if not column_name:
+            raise ValueError("Column name should not be empty") 
+        for row in self.data:
+            if row[column_name].strip():
+                try:
+                    float(row[column_name]) and float(row[column_name]) >= 0.0
+                except ValueError:
+                    return False
+        return True
+
 
     def sum_column(self, column_name):
+        if not column_name:
+            raise ValueError("Column name should not be empty")
         total_sum = 0
         for row in self.data:
             try:
                 total_sum += float(row[column_name])
             except ValueError:
-                pass
+                raise ValueError("Column should contain only numeric values")
         return total_sum
+    
 
     def find_min(self, column_name):
         values = [float(row[column_name]) for row in self.data if row[column_name].strip()]
-        return min(values)
+        minimum = min(values)
+        
+        assert minimum != None, "Minimum should not be None"
+
+        return minimum
+
 
     def find_max(self, column_name):
-        values = [float(row[column_name]) for row in self.data if row[column_name].strip()]
-        return max(values)
+        values = [float(row[column_name]) for row in self.data  if row[column_name].strip()]
+        maximum = max(values)
+
+        assert maximum != None, "Maximum should not be None"
+
+        return maximum
+
 
     def find_avg(self, column_name):
         values = [float(row[column_name]) for row in self.data if row[column_name].strip()]
-        return statistics.mean(values)
+        avg = statistics.mean(values)
+
+        assert avg != None, "Average should not be None"
+
+        return avg
+
 
     def find_shortest_string(self, column_name):
         values = [row[column_name] for row in self.data if row[column_name].strip()]
-        return min(values, key=len)
+        shortest_string = min(values, key=len)
+
+        assert shortest_string != None, "Shortest string should not be None"
+
+        return shortest_string
+
 
     def find_longest_string(self, column_name):
         values = [row[column_name] for row in self.data if row[column_name].strip()]
-        return max(values, key=len)
+        longest_strng = max(values, key=len)
+
+        assert longest_strng != None, "Longest string should not be None"
+
+        return longest_strng
--- tests/test_csv_parser.py
+++ tests/test_csv_parser.py
@@ -1,29 +1,219 @@
 import pytest
-from app.csv_parser import CSVParser
+import csv
+from statistics import StatisticsError
+from app.csv_parser import CSVParser, parse_csv
+# FILE_PATH = "./data/example.csv"
 
-FILE_PATH = "./data/example.csv"
+CSV_DATA = """Name,City,Age
+Simeon,Sofia,25
+Viktor,Plovdiv,35
+Bobi,Veliko Tarnovo,39
+"""
+
+INVALID_AGE = [
+    {"Name": "Simeon", "City": "Sofia", "Age": ""},
+    {"Name": "Viktor", "City": "Plovdiv", "Age": ""},
+    {"Name": "Bobi", "City": "Veliko Tarnovo", "Age": ""},
+]
+
+INVALID_STRINGS = [
+    {"Name": "", "City": "", "Age": "25"},
+    {"Name": "", "City": "", "Age": "35"},
+    {"Name": "", "City": "", "Age": "39"},
+]
+
 
 @pytest.fixture
-def parser():
-    return CSVParser(FILE_PATH)
+def mock_open(mocker):
+    mocker_open_csv = mocker.mock_open(read_data=CSV_DATA)
+    mocker.side_effect = csv.Error("File is not a valid CSV file")
+    mocker.patch("builtins.open", mocker_open_csv)
+
+
+def test_parse_csv_with_fixture(mocker, mock_open):
+    data = parse_csv("file_path.csv")
+    expected_data = [
+        {"Name": "Simeon", "City": "Sofia", "Age": "25"},
+        {"Name": "Viktor", "City": "Plovdiv", "Age": "35"},
+        {"Name": "Bobi", "City": "Veliko Tarnovo", "Age": "39"},
+    ]
+
+    assert data == expected_data
+
+
+@pytest.fixture
+def parser(mocker, mock_open):
+    return CSVParser(parse_csv("file_path.csv"))
+
+
+def test_parse_csv_with_invalid_file():
+    with pytest.raises(ValueError) as e:
+        parse_csv("file_path.txt")
+
+    assert e.type == ValueError
+
+
+def test_parse_csv_with_empty_file():
+    with pytest.raises(ValueError) as e:
+        parse_csv("")
+
+    assert e.type == ValueError
+
 
 def test_count_rows(parser):
-    assert parser.count_rows() == 4
+    assert parser.count_rows() == 3
 
-def test_sum_column(parser):
-    assert parser.sum_column("Age") == 118.0
 
-def test_find_min(parser):
-    assert parser.find_min("Age") == 25.0
+def test_count_rows_throws_exception(parser):
+    parser.data = [
+        {"Name": "Simeon", "City": "Sofia", "Age": "25"},
+        {"Name": "Viktor", "City": "Plovdiv", "Age": "35"},
+    ]
+    with pytest.raises(AssertionError) as e:
+        parser.count_rows()
+
+    assert e.type == AssertionError
+
+
+def test_count_columns(parser):
+    assert parser.count_columns() == 3
+
+
+def test_count_columns_throws_exception(parser):
+    parser.data = [
+        {"Name": "Simeon", "Age": "25"},
+        {"Name": "Viktor", "Age": "35"},
+        {"Name": "Bobi", "Age": "39"},
+    ]
+    with pytest.raises(AssertionError) as e:
+        parser.count_columns()
+
+    assert e.type == AssertionError
+
+
+def test_column_exists(parser):
+    assert parser.column_exists("Name") is True
+    assert parser.column_exists("Age") is True
+    assert parser.column_exists("City") is True
+    assert parser.column_exists("Country") is False
+
+
+def test_column_exists_with_empty_column_name(parser):
+    with pytest.raises(ValueError) as e:
+        parser.column_exists("")
+
+    assert e.type == ValueError
+
+
+def test_column_is_numeric(parser, benchmark):
+    result = benchmark(parser.column_is_numeric, "Age")
+    assert result is True
+
+
+def test_column_is_numeric_with_empty_column_name(parser):
+    with pytest.raises(ValueError) as e:
+        parser.column_is_numeric("")
+
+    assert e.type == ValueError
+
+
+def test_sum_column(parser, benchmark):
+    result = benchmark(parser.sum_column, "Age")
+    assert result == 99.0
+
+
+def test_sum_column_with_empty_column_name(parser):
+    with pytest.raises(ValueError) as e:
+        parser.sum_column("")
+
+    assert e.type == ValueError
+
+
+def test_sum_column_throws_exception(parser):
+    with pytest.raises(ValueError) as e:
+        parser.sum_column("Name")
+        parser.sum_column("City")
+
+    assert e.type == ValueError
+
+
+def test_find_min(parser, benchmark):
+    result = benchmark(parser.find_min, "Age")
+    assert result == 25.0
+
 
 def test_find_max(parser):
-    assert parser.find_max("Age") == 35.0
+    assert parser.find_max("Age") == 39.0
+
 
 def test_find_avg(parser):
-    assert parser.find_avg("Age") == 29.5
+    assert parser.find_avg("Age") == 33.0
+
+
+def test_find_min_throws_exception(parser):
+    with pytest.raises(ValueError) as e:
+        parser.find_min("City")
+
+    assert e.type == ValueError
+
+
+def test_find_max_throws_exception(parser):
+    with pytest.raises(ValueError) as e:
+        parser.find_max("City")
+
+    assert e.type == ValueError
+
+
+def test_find_avg_throws_exception(parser):
+    with pytest.raises(ValueError) as e:
+        parser.find_avg("Name")
+
+    assert e.type == ValueError
+
+
+def test_find_min_with_empty_values(parser):
+    parser.data = INVALID_AGE
+    with pytest.raises(ValueError) as e:
+        parser.find_min("Age")
+
+    assert e.type == ValueError
+
+
+def test_find_max_with_empty_values(parser):
+    parser.data = INVALID_AGE
+    with pytest.raises(ValueError) as e:
+        parser.find_max("Age")
+
+    assert e.type == ValueError
+
+
+def test_find_avg_with_empty_values(parser):
+    parser.data = INVALID_AGE
+    with pytest.raises(StatisticsError) as e:
+        parser.find_avg("Age")
+
+    assert e.type == StatisticsError
+
 
 def test_find_shortest_string(parser):
-    assert parser.find_shortest_string("Name") == "Bob"
+    assert parser.find_shortest_string("Name") == "Bobi"
+
 
 def test_find_longest_string(parser):
-    assert parser.find_longest_string("City") == "San Francisco"
+    assert parser.find_longest_string("City") == "Veliko Tarnovo"
+
+
+def test_find_shortest_string_with_empty_values(parser):
+    parser.data = INVALID_STRINGS
+    with pytest.raises(ValueError) as e:
+        parser.find_shortest_string("Name")
+
+    assert e.type == ValueError
+
+
+def test_find_longest_string_with_empty_values(parser):
+    parser.data = INVALID_STRINGS
+    with pytest.raises(ValueError) as e:
+        parser.find_longest_string("Name")
+
+    assert e.type == ValueError
